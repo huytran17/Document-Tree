49572b73086fbb91ec20240a9c1dced7fa3795d0
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-10-26 16:59:14 +0700

    init proj

---
+++ client/components/Tutorial.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>Hello: {{ this.hello }}</h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      hello: "",
+    };
+  },
+
+  async beforeCreate() {
+    await axios
+      .get("http://localhost:3000/api/greeter/hello")
+      .then((response) => {
+        this.hello = response.data;
+      });
+  },
+};
+</script>
