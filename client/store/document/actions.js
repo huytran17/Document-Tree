595f89679a9e7cffbe5e5ff5df8edb59c7800870
hahaha1618 <alpha.lloyd1368@gmail.com>
2021-11-02 15:04:10 +0700

    directory vuex

--- client/store/document/actions.js
+++ client/store/document/actions.js
@@ -2,9 +2,36 @@ const { CONFIG } = require('../../config/app')
 
 export default {
     async fetchDocuments({ commit }) {
-        await this.$axios.$get((`${CONFIG.BASE_URL}/api/documents/list`)
+        await this.$axios
+            .$get(`${CONFIG.BASE_URL}/api/documents/list`)
             .then((res) => {
-                commit('reloadDocuments', res.data)
-            }))
+                commit('refreshDocuments', res.data)
+            });
+    },
+
+    getRootNodes({ commit }) {
+        commit('getRootNodes')
+    },
+
+    createDocumentTree({ state, dispatch, commit }, payload) {
+        if (payload.documentsTree.length === 0) return null;
+
+        payload.documentsTree.forEach((parentDocument, k) => {
+            commit('initialChildrenDocument', parentDocument)
+
+            for (var i = 0; i < payload.documents.length; i++) {
+                if (payload.documents[i].documentId === payload.documentsTree[k].id) {
+                    commit('addChildrenDocument', { parentDocument, childDocument: payload.documents[i] })
+                    commit('spliceDocuments', i)
+                    --i;
+                }
+            }
+
+            return dispatch('createDocumentTree',
+                {
+                    documentsTree: payload.documentsTree[k].children,
+                    documents: state.documents
+                });
+        });
     }
 }
--- client/store/document/mutations.js
+++ client/store/document/mutations.js
@@ -1,5 +1,33 @@
 export default {
     reloadDocuments(state, documents) {
-        state.documents = documents;
-    }
+        refreshData(state);
+        state.documents = documents
+    },
+
+    getRootNodes(state) {
+        for (var i = 0; i < state.documents.length; i++) {
+            if (!state.documents[i].documentId) {
+                state.documentsTree.push(state.documents[i]);
+                state.documents.splice(i, 1);
+                --i;
+            }
+        }
+    },
+
+    initialChildrenDocument(state, parentDocument) {
+        parentDocument.children = []
+    },
+
+    addChildrenDocument(state, payload) {
+        payload.parentDocument.children.push(payload.childDocument)
+    },
+
+    spliceDocuments(state, position) {
+        state.documents.splice(position, 1)
+    },
+}
+
+function refreshData(state) {
+    state.documents = [];
+    state.documentsTree = [];
 }
